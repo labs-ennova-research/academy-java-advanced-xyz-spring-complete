c7cd141f40d7a70b906a3ec285f8c9e02e07ea9d
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2021-09-30 17:56:07 +0200

    ++test class

---
+++ src/test/java/com/ennova_research/academy/xyzspring/AppTest.java
@@ -0,0 +1,19 @@
+package com.ennova_research.academy.xyzspring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author Alberto Ielpo
+ */
+public class AppTest {
+
+	/**
+	 * 
+	 */
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
